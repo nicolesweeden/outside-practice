356961bbe290e1b06e4adda8744c53b3eba44fc0
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-11-02 23:58:29 -0600

    completed setup for assessment practice

--- assessment 2 practice.js
+++ assessment 2 practice.js
@@ -17,3 +17,28 @@ apple.buy = function () {
 }
 
 apple.buy();
+
+// 2. Write a function that takes in two string arguments: the first being the string assessed, and the second being a single letter. The function should return a count of the occurances of the letter (second argument) within the string (first argument). This function should count both upper and lowercase instances of the letter.
+
+
+
+
+// 3. Write a function that takes in an array of grocery item objects and returns the object with the highest weightInGrams property.
+
+
+
+
+// 4. Write a function that takes in an array of numbers and returns an array of numbers from the original array that were evenly divisible by 13. If a number is not evenly divizible by 13, it should not be returned. If no elements within the array are evenly divisible by 13, the function should return an empty array.
+
+
+
+
+
+
+// 5. Write a function that takes in a string and returns an object with the following properties:
+//     1. string which is to be set the to the argument passed into the function.
+//     2. numberOfWords which is to be set to the number of words within the string.
+//     3. instancesOfE which to be set to the number of instances of the letter "E" within
+//     the argument passed into the function. The count should refelect instances of both
+//     uppercase and lowercase "E" (HINT: a previous function maybe useful in achieving
+//     this).
