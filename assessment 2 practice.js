2d80addb61bf743972b9df43cedc3828c543a13d
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-11-02 23:53:42 -0600

    finished first practice problem

--- assessment 2 practice.js
+++ assessment 2 practice.js
@@ -3,3 +3,17 @@
 // what is a branch on github
 // what is github
 // point of pull requests on github
+
+
+// 1. Write a function that takes in a grocery item object and returns a string representing the price per item for that object.
+
+
+let apple = {};
+apple.price = "2"
+apple.quantity = "1"
+
+apple.buy = function () {
+    alert("Total cost is $" + (apple.price * apple.quantity));
+}
+
+apple.buy();
